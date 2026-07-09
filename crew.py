a1548a1272490901598e21ca87e934a13ed8ef25
Yash Singh <92145289+Mrhacker6@users.noreply.github.com>
2026-07-09 15:32:17 +0530

    Update crew.py

--- crew.py
+++ crew.py
@@ -3,13 +3,16 @@ from crewai import Crew, Process
 from agents import create_agents
 from Task import create_tasks
 from tools import get_yt_tool
+import os
+import streamlit as st
 
+api_key = os.getenv("GOOGLE_API_KEY") or st.secrets["GOOGLE_API_KEY"]
 
 def blog(channel_url, topic):
     """
     Creates a dynamic Crew for any YouTube channel.
     """
-    yt_tool = get_yt_tool(channel_url)
+    yt_tool = get_yt_tool(channel_url,api_key)
     blog_researcher, blog_writer = create_agents(yt_tool)
     researcher_task, writer_task = create_tasks(
         blog_researcher,
