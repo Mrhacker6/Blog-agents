10c2654d7cc257992996afe206a10eadef3c749f
Yash Singh <92145289+Mrhacker6@users.noreply.github.com>
2026-07-09 14:43:39 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,4 +1,15 @@
 from crewai_tools import YoutubeChannelSearchTool
 
-def get_yt_tool(channel_url):
-    return YoutubeChannelSearchTool(youtube_channel_url=channel_url)
+def get_yt_tool(channel_url, api_key):
+    return YoutubeChannelSearchTool(
+        youtube_channel_url=channel_url,
+        config={
+            "llm": {
+                "provider": "google",
+                "config": {
+                    "model": "gemini-2.5-flash",
+                    "api_key": api_key,
+                },
+            }
+        },
+    )
